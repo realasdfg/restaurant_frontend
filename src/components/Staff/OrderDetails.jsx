854070cb86f58a179e9bfd551773a1c9bf0a6127
Roman <realasdfga@gmail.com>
2025-01-30 00:40:35 +0200

    Polish translate order details

--- src/components/Staff/OrderDetails.jsx
+++ src/components/Staff/OrderDetails.jsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { fetchOrderById } from '../../services/api';
+import React, {useEffect, useState} from 'react';
+import {fetchOrderById, fetchTableById} from '../../services/api';
 import LoadingSpinner from '../shared/LoadingSpinner';
+import {Button, Table} from "antd";
 
-const OrderDetails = ({ orderId }) => {
+const OrderDetails = ({orderId}) => {
     const [order, setOrder] = useState(null);
+    const [table, setTable] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const fetchOrder = async () => {
+        const fetchData = async () => {
             try {
-                const response = await fetchOrderById(orderId);
-                setOrder(response.data);
-                console.log(response.data)
+                const orderResponse = await fetchOrderById(orderId);
+                setOrder(orderResponse.data);
+                if (orderResponse.data.table_id) {
+                    const tableResponse = await fetchTableById(orderResponse.data.table_id);
+                    setTable(tableResponse.data);
+                }
             } catch (error) {
                 console.error('Error fetching order details:', error);
             } finally {
@@ -19,19 +24,22 @@ const OrderDetails = ({ orderId }) => {
             }
         };
 
-        fetchOrder();
+        fetchData();
     }, [orderId]);
 
-    if (loading) return <LoadingSpinner />;
+    if (loading) return <LoadingSpinner/>;
 
     return (
         <div className="flex justify-center min-h-screen">
             <div className="bg-white p-4 rounded-lg shadow w-full lg:w-3/5 xl:w-2/5">
-                <h2 className="text-xl font-bold">Деталі замовлення #{orderId}</h2>
-                <p><strong>Тип:</strong> {order.type}</p>
-                <p><strong>Стіл:</strong> {order.table_id ? order.table_id : '-'}</p>
-                <p><strong>Дата створення:</strong> {new Date(order.created_at).toLocaleString()}</p>
-                <p><strong>Оплата:</strong> {order.paid ? 'Оплачено' : 'Не оплачено'}</p>
+                <h2 className="text-xl font-bold">Szczegóły zamówienia #{orderId}</h2>
+                <p><strong>Typ:</strong> {order.type === 'dinein' ? 'W restauracji' : 'Na wynos'}</p>
+                <p><strong>Stolik:</strong> {table ? table.name : '-'}</p>
+                <p><strong>Utworzony:</strong> {new Date(order.created_at).toLocaleString()}</p>
+                <Button color="primary" variant="outlined" className="mb-3">Dodaj pozycji</Button>
+                <Table>
+
+                </Table>
             </div>
         </div>
     );
